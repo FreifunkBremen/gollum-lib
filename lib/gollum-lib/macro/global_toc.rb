6903eac99263aec763289ac26dceefba8d25d9b4
mortzu <mortzu@gmx.de>
2019-03-05 18:20:43 +0100

    global_toc.rb: make sidebar navigation tree collapsible

--- lib/gollum-lib/macro/global_toc.rb
+++ lib/gollum-lib/macro/global_toc.rb
@@ -2,10 +2,18 @@ module Gollum
   class Macro
     class GlobalTOC < Gollum::Macro
       def render(title = "Global Table of Contents")
-        if @wiki.pages.size > 0
-          result = '<ul>' + @wiki.pages.map { |p| "<li><a href=\"/#{p.url_path}\">#{p.url_path_display}</a></li>" }.join + '</ul>'
+        pages = @wiki.pages.sort do |a,b|
+          if a.path.include?('/') and b.path.include?('/') then
+            a.path <=> b.path
+          elsif a.path.include?('/') then
+            1
+          elsif b.path.include?('/') then
+            -1
+          else
+            a.path <=> b.path
+          end
         end
-        "<div class=\"toc\"><div class=\"toc-title\">#{title}</div>#{result}</div>"
+        Gollum::FileView.new(pages, {:collapse_tree => true}).render_files.gsub('href="', 'href="/')
       end
     end
   end
